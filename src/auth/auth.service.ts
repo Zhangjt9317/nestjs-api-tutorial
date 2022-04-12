b094bc895fa0a505f5b5619ac4c19c7c0439fd2c
Jingtian Zhang <sam.zhang@desty.app>
2022-04-12 09:48:09 +0800

    fix: signup and signin logic in auth.service with validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
+  // dont need to import prisma again, since it is globally imported
+  constructor(private prisma: PrismaService) {}
   signin() {
+    // return the saved user
     return { msg: 'i have signed in' };
   }
 
-  signup() {
-    return { msg: 'i have signed up' };
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    // save the user to the database
+    const user = this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    return user;
   }
 }
